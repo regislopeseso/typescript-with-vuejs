d841e273c82f5d7641380011669f5c56733ecb39
Regis <dev05@sistemaeso.com.br>
2026-03-10 10:05:02 -0300

    Typing Refs

---
+++ src/types/Emoji.ts
@@ -0,0 +1,17 @@
+type Emoji =
+  | "confused"
+  | "cool"
+  | "crying"
+  | "grinning-squinting"
+  | "happy"
+  | "heat-eyes"
+  | "laugh"
+  | "sad"
+  | "shocked"
+  | "smiling"
+  | "smiling-with-eyes"
+  | "surprised"
+  | "wink"
+  | "worried";
+
+export default Emoji;
